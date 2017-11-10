8aef55b2da8c941409d5fe1740b0876376e59eab
Mark Blanco <markb1@andrew.cmu.edu>
2017-11-10 10:03:13 -0500

    Additions to userspace freq governor: cpu usage, sysfs paths, and script to run core at max usage.

--- src/cpu_usage.py
+++ src/cpu_usage.py
@@ -1,34 +1,57 @@
 import time
 import sys, os
-
+import sysfs_paths as sysfs
+# Userspace cpu frequency governor
 # Based on:
 # https://stackoverflow.com/questions/1296703/getting-system-status-in-python#1296816
 
+# On the exynos 5422, cluster 0 is the low-power (LITTLE) cluster and 
+# cluster 1 is the high-power (big) cluster.
+
+#Cluster 0 Up Threshold:
+CL0_UP_THRESH = 0.9
+# Cluster 0 down threshold:
+CL0_DN_THRESH = 0.7
+#Cluster 1 Up Threshold:
+CL1_UP_THRESH = 0.8
+# Cluster 1 Down Threshold:
+CL1_DN_THRESH = 0.5
+# Sampling rate in steps of microseconds (us)
+FREQ_SAMPLING_RATE = 100
+# Interval for sampling CPU load, in seconds
 INTERVAL = 0.1
-def getTimeList():
+def getTimeList(n=None):
 	"""
 	Fetches a list of time units the cpu has spent in various modes
 	Detailed explanation at http://www.linuxhowtos.org/System/procstat.htm
 	"""
-	cpuStats = file("/proc/stat", "r").readline()
-	columns = cpuStats.replace("cpu", "").split(" ")
-	return map(int, filter(None, columns))
+	if n is None:	
+		cpuStats = file("/proc/stat", "r").readline().strip()
+		columns = cpuStats.replace("cpu", "").split(" ")
+		return map(int, filter(None, columns))
+	else:
+		with open("/proc/stat", "r") as procs:
+			# Read out the first line which is aggregate usage
+			cpuStats_l = procs.read().split('\n')
+			cpuStats_l = [e.strip().split(' ')[1:] for e in cpuStats_l]
+			columns = cpuStats_l[n+1]
+			return map(int, filter(None, columns))
 
-def deltaTime(interval):
+def deltaTime(interval, n=None):
 	"""
 	Returns the difference of the cpu statistics returned by getTimeList
 	that occurred in the given time delta
 	"""
-	timeList1 = getTimeList()	
+	timeList1 = getTimeList(n)	
 	time.sleep(interval)
-	timeList2 = getTimeList()
+	timeList2 = getTimeList(n)
 	return [(t2-t1) for t1, t2 in zip(timeList1, timeList2)]
 
-def getCpuLoad():
+def getCpuLoad(n=None):
 	"""
 	Returns the cpu load as a value from the interval [0.0, 1.0]
 	"""
-	dt = list(deltaTime(INTERVAL))
+	dt = list(deltaTime(INTERVAL,n))
 	idle_time = float(dt[3])
 	total_time = sum(dt)
 	load = 1-(idle_time/total_time)
@@ -39,9 +62,19 @@ def getAvailFreqs(cpu_num):
 	freqs = open('/sys/devices/system/cpu/cpufreq/policy{}/scaling_available_frequencies'.format(cluster)).read().split(' ')
 	return freqs
 
-while True:
-	print "CPU usage=%.2f%%" % (getCpuLoad()*100.0)
-	print "Available freqs:\n"
-	print getAvailFreqs(0)
-	print getAvailFreqs(4)
-	time.sleep(0.1)
+def getClusterUsage(cluster_num):
+	with open(sysfs.fn_core_cpus,'r') as affected_cpus:
+		cpus = affected_cpus.read().split()
+		
+
+#def setClusterFreq(cluster_num):
+
+if __name__ == "__main__":
+	while True:
+		print "CPU usage=%.2f%%" % (getCpuLoad()*100.0)
+		for i in range(8):
+			print "CPU {} usage:{}".format(i, getCpuLoad(i)*100.0)
+		#print "Available freqs:\n"
+		#print getAvailFreqs(0)
+		#print getAvailFreqs(4)
+		time.sleep(0.1)
---
+++ src/sysfs_paths.py
@@ -0,0 +1,31 @@
+# This file defines strings as sysfs paths for various
+# dials and control knobs available in Debian.
+# fn_ prefix indicates that the string should be formatted with a
+# core number
+
+# For each cpu core
+fn_cpu_core_base="/sys/devices/system/cpu/cpu{}/cpufreq/"
+fn_cpu_cluster=fn_cpu_core_base+"related_cpus"
+fn_cpu_max_freq=fn_cpu_core_base+"cpuinfo_max_freq"
+fn_cpu_min_freq=fn_cpu_core_base+"cpuinfo_min_freq"
+fn_cpu_freq_read=fn_cpu_core_base+"cpuinfo_cur_freq"
+fn_cpu_governor=fn_cpu_core_base+"scaling_governor"
+fn_cpu_max_freq_set=fn_cpu_core_base+"scaling_max_freq"
+fn_cpu_min_freq_set=fn_cpu_core_base+"scaling_min_freq"
+fn_cpu_freq_set=fn_cpu_core_base+"scaling_cur_freq"
+fn_core_enabled=fn_cpu_core_base[:-8]+"online"
+
+# for clusters (e.g. policies on whole clusters):
+fn_cluster_base="/sys/devices/system/cpu/cpufreq/policy{}/"
+fn_core_max=fn_cluster_base+"cpuinfo_max_freq"
+fn_core_min=fn_cluster_base+"cpuinfo_min_freq"
+fn_core_freq_range=fn_cluster_base+"cpuinfo_available_frequencies"
+fn_core_cpus=fn_cluster_base+"affected_cpus"
+#fn_core_govs=fn_cluster_base+"scaling_cur_governor"
+fn_core_gov=fn_cluster_base+"scaling_governor"
+fn_core_freq_read=fn_cluster_base+"cpuinfo_cur_freq"
+fn_core_freq_set=fn_cluster_base+"scaling_setspeed"
+fn_core_max_set=fn_cluster_base+"scaling_max_freq"
+fn_core_min_set=fn_cluster_base+"scaling_min_freq"
+
+
