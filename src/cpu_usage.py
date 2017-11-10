a1ff41e5b7f397feda5143ce09602dcd0386a906
Mark Blanco <markb1@andrew.cmu.edu>
2017-11-10 10:49:38 -0500

    Userland cpufreq governor works. Bit hacky though.

--- src/cpu_usage.py
+++ src/cpu_usage.py
@@ -1,6 +1,8 @@
 import time
 import sys, os
 import sysfs_paths as sysfs
+import atexit 
+
 # Userspace cpu frequency governor
 # Based on:
 # https://stackoverflow.com/questions/1296703/getting-system-status-in-python#1296816
@@ -20,6 +22,10 @@ CL1_DN_THRESH = 0.5
 FREQ_SAMPLING_RATE = 100
 # Interval for sampling CPU load, in seconds
 INTERVAL = 0.1
+
+prev_gov_0 = 'ondemand'
+prev_gov_1 = 'ondemand'
+
 def getTimeList(n=None):
 	"""
 	Fetches a list of time units the cpu has spent in various modes
@@ -59,21 +65,82 @@ def getCpuLoad(n=None):
 
 def getAvailFreqs(cpu_num):
 	cluster = (cpu_num//4) * 4
-	freqs = open('/sys/devices/system/cpu/cpufreq/policy{}/scaling_available_frequencies'.format(cluster)).read().split(' ')
+	freqs = open(sysfs.fn_core_freq_range.format(cluster)).read().split(' ')
 	return freqs
 
 def getClusterUsage(cluster_num):
-	with open(sysfs.fn_core_cpus,'r') as affected_cpus:
-		cpus = affected_cpus.read().split()
+	with open(sysfs.fn_cluster_cpus.format(cluster_num),'r') as affected_cpus:
+		cpus = [int(x) for x in affected_cpus.read().strip().split(' ')]
+		use = [getCpuLoad(x) for x in cpus]
+		#total_load = sum(use)/float(len(use))
+		return use #total_load
 		
+def setUserSpace():
+	with open(sysfs.fn_cluster_gov.format(0), 'r') as f:
+		prev_gov_0 = f.readline().strip()
+	with open(sysfs.fn_cluster_gov.format(4), 'r') as f:
+		prev_gov_1 = f.readline().strip()
+	with open(sysfs.fn_cluster_gov.format(0), 'w') as f:
+		f.write('userspace')
+	with open(sysfs.fn_cluster_gov.format(4), 'w') as f:
+		f.write('userspace')
 
-#def setClusterFreq(cluster_num):
+def unsetUserSpace():
+	with open(sysfs.fn_cluster_gov.format(0), 'w') as f:
+		f.write(prev_gov_0)
+	with open(sysfs.fn_cluster_gov.format(4), 'w') as f:
+		f.write(prev_gov_1)
+
+def getClusterFreq(cluster_num):
+	with open(sysfs.fn_cluster_freq_read.format(cluster_num), 'r') as f:
+		return int(f.read().strip())
+	
+# Accepts frequency in khz as int or string
+def setClusterFreq(cluster_num, frequency):
+	with open(sysfs.fn_cluster_freq_set.format(cluster_num), 'w') as f:
+		# todo: add bounds checking
+		f.write(str(frequency))	
+
+def lAvg(l):
+	return float(sum(l))/len(l)
+
 
 if __name__ == "__main__":
+	print("Starting userspace cpu frequency governor. Make sure gov is set to userspace!!.")
+	atexit.register(unsetUserSpace)
+	setUserSpace()
 	while True:
-		print "CPU usage=%.2f%%" % (getCpuLoad()*100.0)
-		for i in range(8):
-			print "CPU {} usage:{}".format(i, getCpuLoad(i)*100.0)
+		#print "CPU usage=%.2f%%" % (getCpuLoad()*100.0)
+		#for i in range(8):
+		#	print "CPU {} usage:{}".format(i, getCpuLoad(i)*100.0)
+		# Get per-core cpu util by cluster
+		cl0 = getClusterUsage(0)
+		freq0 = getClusterFreq(0)
+		new_freq0 = freq0
+		if max(cl0) > CL0_UP_THRESH:
+			# increase the frequency
+			new_freq0 =  min(1400000, freq0+int(freq0*0.5))
+			setClusterFreq(0, new_freq0)
+		elif max(cl0) < CL0_DN_THRESH:
+			new_freq0 = max(400000, freq0-int(freq0*0.2))
+			setClusterFreq(0, new_freq0)
+		cl1 = getClusterUsage(4)
+		freq1 = getClusterFreq(4)
+		new_freq1 = freq1
+		if max(cl1) > CL1_UP_THRESH:
+			# increase the frequency
+			new_freq1 = min(2000000, freq1+int(freq1*0.7))
+			setClusterFreq(4, new_freq1)
+		elif max(cl1) < CL1_DN_THRESH:
+			new_freq1 = max(200000, freq1-int(freq1*0.2))
+			setClusterFreq(4, new_freq1)
+
+		print('cluster\told_freq\tnew_freq\ttotal_usage')
+		print('low_power\t{}\t{}\t{}'.format(freq0, new_freq0, float(sum(cl0))/len(cl0)))
+		print('high_power\t{}\t{}\t{}'.format(freq1, new_freq1, float(sum(cl1))/len(cl1)))
+		# todo: use exponential averages over time
+		# todo: take temperature ceiling into account
+		# todo: refactor to improve performance/overhead	
 		#print "Available freqs:\n"
 		#print getAvailFreqs(0)
 		#print getAvailFreqs(4)
