1784c62271c29d55a392aafcf563a8f7c67bccd9
Mark Blanco <markb1@andrew.cmu.edu>
2017-12-01 00:00:59 -0500

    Add start of power model scrtip.

---
+++ src/power_model.py
@@ -0,0 +1,10 @@
+import cpu_usage
+import psutil
+
+# leakage power for a given resource (specified by the input model parameters)
+def resLeakagePower(
+
+def resPower()
+
+if __name__=="__main__":
+	
