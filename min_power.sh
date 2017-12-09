288806983ccf5639885a2152eed53f013a820bdf
Mark Blanco <markb1@andrew.cmu.edu>
2017-12-09 17:25:38 -0500

    Permission fix.

--- min_power.sh
+++ min_power.sh
@@ -4,11 +4,11 @@ echo "userspace" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo "userspace" > /sys/devices/system/cpu/cpufreq/policy4/scaling_governor
 
 # Disable some cores
-echo "0" > /sys/devices/system/cpu/cpu0/online
-echo "0" > /sys/devices/system/cpu/cpu1/online
-echo "0" > /sys/devices/system/cpu/cpu2/online
-echo "0" > /sys/devices/system/cpu/cpu4/online
-echo "0" > /sys/devices/system/cpu/cpu5/online
-echo "0" > /sys/devices/system/cpu/cpu6/online
+#echo "0" > /sys/devices/system/cpu/cpu0/online
+#echo "0" > /sys/devices/system/cpu/cpu1/online
+#echo "0" > /sys/devices/system/cpu/cpu2/online
+#echo "0" > /sys/devices/system/cpu/cpu4/online
+#echo "0" > /sys/devices/system/cpu/cpu5/online
+#echo "0" > /sys/devices/system/cpu/cpu7/online
 
 
