d1410300a5f43ef2d6102fe6b90b1cb7a299b0c7
Mark Blanco <mark.p.blanco@gmail.com>
2017-12-09 20:01:22 -0500

    PAPI stuff.

--- src/PAPI/pypapi_test.py
+++ src/PAPI/pypapi_test.py
@@ -1,2 +1,7 @@
 from pypapi import papi_high as ph
-from pypai import events as pe
+from pypapi import events as pe
+
+# high level api test:
+print("Testing papi_high")
+flops = ph.flops()
+print("Flops:", flops)
