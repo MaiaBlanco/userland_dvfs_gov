7bc574938b9fbaae8ef64ac4950aa863c5dc2dfe
Mark Blanco <mark.p.blanco@gmail.com>
2017-12-10 01:15:45 -0500

    update pypapi test script.

--- src/PAPI/pypapi_test.py
+++ src/PAPI/pypapi_test.py
@@ -1,7 +1,24 @@
 from pypapi import papi_high as ph
-from pypapi import events as pe
+from pypapi import papi_low as papi
+from pypapi import events
 
 # high level api test:
 print("Testing papi_high")
-flops = ph.num_counters()
-print("Num Counters:", flops)
+cntrs = ph.num_counters()
+print("Num Counters:", cntrs)
+
+papi.library_init()
+
+evs = papi.create_eventset()
+papi.add_event(evs, events.PAPI_TOT_CYC)
+papi.add_event(evs, events.PAPI_L1_DCM)
+
+papi.start(evs)
+
+# Do some computation here
+
+result = papi.stop(evs)
+print(result)
+
+papi.cleanup_eventset(evs)
+papi.destroy_eventset(evs)
