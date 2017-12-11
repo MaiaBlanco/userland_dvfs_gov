ff954da5a23f49759c5a7472127fce1fe7eda784
Mark Blanco <markb1@andrew.cmu.edu>
2017-12-11 03:34:20 -0500

    Update stats logging script to include estimates on power. Add preliminary instruction power log.

--- src/temp_load_power_logging.py
+++ src/temp_load_power_logging.py
@@ -3,9 +3,14 @@ import time
 import cpu_usage
 import telnetlib as tel
 import atexit
+import power_model as pw
+import therm_params as tp
 
 # Time to wait between logging a line in ms (target, not guaranteed)
 DELAY=0.1
+# power used by ethernet and wifiusb when active (assuming they are active)
+board_power = 1.71
+peripheral_power = 0.072 + 0.472 + board_power
 out_file = None
 
 header = "time watts usage_c0 usage_c1 usage_c2 usage_c3 usage_c4 usage_c5 usage_c6 usage_c7 temp4 temp5 temp6 temp7 temp_gpu freq_little_cluster freq_big_cluster freq_gpu freq_mem volts_little_cluster volts_big_cluster volts_gpu volts_mem"
@@ -37,6 +42,12 @@ if __name__ == "__main__":
 	while True:	
 		last_time = time.time()#time_stamp
 		temps = cpu_usage.getTemps()
+		# Set temperatures for the small, big, gpu and memory. Convert to kelvin
+		T = [0.0]*4
+		T[0] = sum(temps)/len(temps)+ 273.15
+		T[1] = max(temps[0:4]) 	+ 273.15
+		T[2] = temps[4] 		+ 273.15
+		T[3] = max(temps[0:4]) 	+ 273.15
 		# -1 indicates get loads for all cpus
 		# 0.05 indicates s interval for measurement
 		usages = cpu_usage.getCpuLoad(-1, 0.05)	
@@ -64,16 +75,32 @@ if __name__ == "__main__":
 				total_power = 0
 			else:
 				total_power = float(ln[-2])
-			
+		
+
+		# Now compute the leakage power of each resource
+		Pl = [0.0, 0.0, 0.0, 0.0]
+		#print("Volts: {}".format(V))
+		#print("Hz: {}".format(F))
+		#print("Temp: {}".format(T))
+		total_leakage_power = 0.0
+		Pl[0] = pw.leakagePower(tp.c1, tp.c2, tp.Igate, V[0], T[0])
+		Pl[1] = pw.leakagePower(tp.c1, tp.c2, tp.Igate, V[1], T[1])
+		Pl[2] = pw.leakagePower(tp.c1, tp.c2, tp.Igate, V[2], T[2])
+		Pl[3] = pw.leakagePower(tp.c1, tp.c2, tp.Igate, V[3], T[3])
+		total_leakage_power = sum(Pl)
+		total_dynamic_power = total_power - total_leakage_power - peripheral_power
+
+	
 		time_stamp = last_time
 		# Data writeout:
-		out_ln = "{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}".format(\
-			time_stamp, total_power, \
+		fmt_str = "{}\t"*26
+		out_ln = fmt_str.format(\
+			time_stamp, total_power, total_leakage_power, total_dynamic_power, peripheral_power,\
 			usages[0], usages[1], usages[2], usages[3], \
 			usages[4], usages[5], usages[6], usages[7],\
 			temps[0], temps[1], temps[2], temps[3], temps[4], \
 			F[0], F[1], F[2], F[3],
-			V[0], V[1], V[2], V[3]
+			V[0], V[1], V[2], V[3],
 			)
 		print(out_ln)
 		if not out_file is None:
