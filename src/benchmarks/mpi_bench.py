ae505b1c3af242db2e61c6811bda831559425287
Mark Blanco <mark.p.blanco@gmail.com>
2017-12-11 05:30:29 -0500

    changes.

---
+++ src/benchmarks/mpi_bench.py
@@ -0,0 +1,9 @@
+import mpi4py as mpi
+import time
+
+class Worker:
+	# define things here...
+
+class Master:
+	# define the master here...
+	# Maybe should do some logging of statistics while the workers run?
