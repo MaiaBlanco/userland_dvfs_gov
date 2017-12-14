9ec222e0dc5a418aca231cda1469a889546dc181
Mark Blanco <markb1@andrew.cmu.edu>
2017-12-14 03:03:56 -0500

    Bug fix for thermal ondemand governor.

--- src/gov/ondemand_userspace_thermal.py
+++ src/gov/ondemand_userspace_thermal.py
@@ -6,16 +6,19 @@ import os
 import sysfs_paths as sysfs
 import math
 import atexit
+import psutil
 
 # Thermal threshold in celsius
 THERMAL_THRESH = 70
 CLUSTER_UP_THRESH = 0.8
-LOAD_TARGET = CLUSTER_UP_THRESH
+LOAD_TARGET = 50
 # Sampling rate in steps of microseconds (us)
-FREQ_SAMPLING_RATE = 100
 CLUSTER_SIZE = 4
-REFRESH_PERIOD = 0.1
+REFRESH_PERIOD = 0.15
 MAX_THERMAL_FREQ_INDEX = 0
+# CPU usage polling period MUST be less than or equal to 
+# the refresh period. Minimum suggested in psutil docs is 0.1 s
+CPU_USAGE_PERIOD = REFRESH_PERIOD - 0.005 
 
 def usage():
 	print("USAGE: {} [options]", sys.argv[0])
@@ -46,13 +49,14 @@ if __name__ == "__main__":
 		except:
 			usage()
 	print("Starting userspace ondemand with thermal limit of {} celsius.".format(THERMAL_THRESH))
-	print("WRNING: thermal limits are only in effect for the big cluster.")
+	print("WARNING: thermal limits are only in effect for the big cluster.")
 	atexit.register(sysfs_utils.unsetUserSpace, clusters=clusters)
 	sysfs_utils.setUserSpace(clusters=clusters)
 	avail_freqs = [list()] * (sorted(clusters)[-1] + 1)
 	sel_cluster_freq = [0] * (sorted(clusters)[-1] + 1)
 	for cluster in clusters:
 		avail_freqs[cluster] = sysfs_utils.getAvailFreqs(cluster)
+		print("{} frequencies for cluster {}".format(len(avail_freqs[cluster]), cluster))
 	Fs = [[0]] * (sorted(clusters)[-1] + 1)
 	Fs_new =  [[0]] * (sorted(clusters)[-1] + 1)
 	U = [0.0] * (sorted(clusters)[-1] + 1)
@@ -60,18 +64,22 @@ if __name__ == "__main__":
 	while True:
 		last_time = time.time()
 		# Get the latest cpu usages
-		cpu_loads = sysfs_utils.getCpuLoad(n=-1, interval=0.05)
+		#cpu_loads = sysfs_utils.getCpuLoad(n=-1, interval=CPU_USAGE_PERIOD)
+		cpu_loads = [ x/100 for x in \
+			psutil.cpu_percent(interval=CPU_USAGE_PERIOD, percpu=True)]
 		for cluster in clusters:
 			if cluster == 4:
 				# get the first four temp which correspond to the big cluster cores
 				T = sysfs_utils.getTemps()[0:4]
 				if max(T) >= THERMAL_THRESH:
 					MAX_THERMAL_FREQ_INDEX = max(MAX_THERMAL_FREQ_INDEX - 1, 0)
+					#print("Tripped thermal limit. ({} > {})".format(max(T), THERMAL_THRESH))
 				else:
 					MAX_THERMAL_FREQ_INDEX = len(avail_freqs[cluster])-1
 			if max(cpu_loads[cluster:(cluster+CLUSTER_SIZE)]) > CLUSTER_UP_THRESH:
 				# increase the cluster frequency to max
 				sel_cluster_freq[cluster] = MAX_THERMAL_FREQ_INDEX
+				#print("Tripped load limit. ({} > {})".format(max(cpu_loads[cluster:(cluster+CLUSTER_SIZE)]), CLUSTER_UP_THRESH))
 			else:
 				# find a frequency that will maintain no more than LOAD_TARGET usage on any core.
 				Fs[cluster] = sysfs_utils.getClusterFreq(cluster)
@@ -79,12 +87,22 @@ if __name__ == "__main__":
 													LOAD_TARGET, Fs[cluster])
 				# Search up to and including the current frequency for one that maintains the 
 				# desired load:
-				for index in range(	len(avail_freqs[cluster]) ):
-					if avail_freqs[cluster][index] >= Fs_new[cluster]:
-						sel_cluster_freq[cluster] = min(index, MAX_THERMAL_FREQ_INDEX)
+				for index,frequency in enumerate(avail_freqs[cluster]):
+					if frequency == Fs_new[cluster]:
+						sel_cluster_freq[cluster] = index
+					elif frequency > Fs_new[cluster]:
+						sel_cluster_freq[cluster] = max(index-1, 0)
 						break
-			sysfs_utils.setClusterFreq(cluster, \
-				avail_freqs[cluster][sel_cluster_freq[cluster]])
+					elif index >= MAX_THERMAL_FREQ_INDEX:
+						sel_cluster_freq[cluster] = MAX_THERMAL_FREQ_INDEX
+						break
+			selected_index = sel_cluster_freq[cluster]
+			try:
+				sysfs_utils.setClusterFreq(cluster, \
+				avail_freqs[cluster][selected_index])
+			except:
+				print("ERROR: tried to access {} for cluster {}".format(selected_index, cluster))
+				print(avail_freqs[cluster])
 		time.sleep( max(0, REFRESH_PERIOD - ( time.time() - last_time ) ) )
 		#print("period {} s".format( time.time() - last_time ) )
 
