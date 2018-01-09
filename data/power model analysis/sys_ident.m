84537f1364f8b5bcf98f27e3f8acb87c563545bd
Mark Blanco <mark.p.blanco@gmail.com>
2018-01-09 09:52:38 -0500

    Add more verification data for power model.

--- data/power model analysis/sys_ident.m
+++ data/power model analysis/sys_ident.m
@@ -1,4 +1,7 @@
 data = csvread('matlab_temp_sys_identification.csv',1);
+verification_data = csvread('matlab_temp_sys_identification_verification_blackscholes.csv',1);
+v_thermal_inputs = verification_data(:,2:5);
+v_power_inputs = verification_data(:,10:17);
 thermal_inputs = data(:,2:5);
 power_inputs = data(:,8:15);
 time_data = iddata(thermal_inputs, power_inputs, 0.2);
@@ -6,6 +9,11 @@ time_data.InputName = {'watt0', 'watt1', 'watt2', 'watt3', 'watt4', 'watt5', 'wa
 time_data.OutputName = {'temp4', 'temp5', 'temp6', 'temp7'};
 time_data.InputUnit = {'Watts','Watts','Watts','Watts','Watts','Watts','Watts','Watts'};
 time_data.OutputUnit = {'Celcius','Celcius','Celcius','Celcius'};
+v_time_data = iddata(v_thermal_inputs, v_power_inputs, 0.2);
+v_time_data.InputName = {'watt0', 'watt1', 'watt2', 'watt3', 'watt4', 'watt5', 'watt6', 'watt7'};
+v_time_data.OutputName = {'temp4', 'temp5', 'temp6', 'temp7'};
+v_time_data.InputUnit = {'Watts','Watts','Watts','Watts','Watts','Watts','Watts','Watts'};
+v_time_data.OutputUnit = {'Celcius','Celcius','Celcius','Celcius'};
 nx=4;%1:10;
 
 mp = ssest(time_data, nx, 'Ts', 0.2);%, 'DisturbanceModel', 'none');
@@ -19,5 +27,10 @@ mp = ssest(time_data, nx, 'Ts', 0.2);%, 'DisturbanceModel', 'none');
 % mp = ssest(time_data, initial_system, opts_sys);
 
 A_s = mp.C*(mp.A*0.2+eye(4))*mp.C^(-1);
+A_s = A_s / norm(A_s, inf);
 B_s = mp.C * mp.B * 0.2;
 
+tp = zeros(size(v_thermal_inputs));
+for i = 1:size(v_thermal_inputs, 1)
+    tp(i, :) = A_s * v_thermal_inputs(i, :)' + B_s * v_power_inputs(i, :)'; 
+end
