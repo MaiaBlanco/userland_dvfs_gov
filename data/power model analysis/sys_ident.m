e62f84dc3b886ad4c739d9d36851c8d5f96773e6
Mark <mrkblnc1248@gmail.com>
2018-01-11 01:02:11 -0500

    Update MATLAB code to have a temporal downscaling parameter.

--- data/power model analysis/sys_ident.m
+++ data/power model analysis/sys_ident.m
@@ -1,22 +1,26 @@
+SKIP_FACTOR = 3;
 data = csvread('matlab_temp_sys_identification.csv',1);
 verification_data = csvread('matlab_temp_sys_identification_verification_blackscholes.csv',1);
-v_thermal_inputs = verification_data(:,2:5);
-v_power_inputs = verification_data(:,10:17);
-thermal_inputs = data(:,2:5);
-power_inputs = data(:,8:15);
-time_data = iddata(thermal_inputs, power_inputs, 0.2);
-time_data.InputName = {'watt0', 'watt1', 'watt2', 'watt3', 'watt4', 'watt5', 'watt6', 'watt7'};
+v_thermal_inputs = verification_data(1:SKIP_FACTOR:end,2:5);
+% v_power_inputs = verification_data(:,10:17);
+v_power_inputs = verification_data(1:SKIP_FACTOR:end,14:17);
+thermal_inputs = data(1:SKIP_FACTOR:end,2:5);
+% power_inputs = data(,8:15);
+power_inputs = data(1:SKIP_FACTOR:end,12:15);
+time_data = iddata(thermal_inputs, power_inputs, 0.2*SKIP_FACTOR);
+% 'watt0', 'watt1', 'watt2', 'watt3', 
+time_data.InputName = {'watt4', 'watt5', 'watt6', 'watt7'};
 time_data.OutputName = {'temp4', 'temp5', 'temp6', 'temp7'};
-time_data.InputUnit = {'Watts','Watts','Watts','Watts','Watts','Watts','Watts','Watts'};
+time_data.InputUnit = {'Watts','Watts','Watts','Watts'};
 time_data.OutputUnit = {'Celcius','Celcius','Celcius','Celcius'};
-v_time_data = iddata(v_thermal_inputs, v_power_inputs, 0.2);
-v_time_data.InputName = {'watt0', 'watt1', 'watt2', 'watt3', 'watt4', 'watt5', 'watt6', 'watt7'};
+v_time_data = iddata(v_thermal_inputs, v_power_inputs, 0.2*SKIP_FACTOR);
+v_time_data.InputName = {'watt4', 'watt5', 'watt6', 'watt7'};
 v_time_data.OutputName = {'temp4', 'temp5', 'temp6', 'temp7'};
-v_time_data.InputUnit = {'Watts','Watts','Watts','Watts','Watts','Watts','Watts','Watts'};
+v_time_data.InputUnit = {'Watts','Watts','Watts','Watts'};
 v_time_data.OutputUnit = {'Celcius','Celcius','Celcius','Celcius'};
 nx=4;%1:10;
 
-mp = ssest(time_data, nx, 'Ts', 0.2);%, 'DisturbanceModel', 'none');
+mp = ssest(time_data, nx, 'Ts', 0.2*SKIP_FACTOR);%, 'DisturbanceModel', 'none');
 
 % initial_system = idss(0.9 * eye(4), 0.9 * ones(4,8), eye(4), zeros(4,8) );
 % initial_system.Structure.C.Free = false;
