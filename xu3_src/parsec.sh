7f36244b4cb0485ed5fb042b03cada4520d6e3a6
MarkBlanco <markb1@andrew.cmu.edu>
2018-02-12 17:05:24 +0000

    Update random loads script to use parsec executables provided to students.

--- xu3_src/parsec.sh
+++ xu3_src/parsec.sh
@@ -1,1 +1,2 @@
-sudo /home/odroid/PARSEC/parsec-3.0/bin/parsecmgmt -a run -p bodytrack -i $1 -n $2 -c gcc-openmp -s "sudo taskset --all-tasks $3"
+echo running: taskset --all-tasks $1 $2 $3 $4 
+taskset --all-tasks $1 $2 $3 $4
--- xu3_src/random_loads.py
+++ xu3_src/random_loads.py
@@ -10,9 +10,11 @@ bench_thresh = 0.2
 period_max = 30
 bench_processes = []
 other_processes = []
-input_size = ["native"]#, "simlarge", "simsmall"]
-path = "/home/odroid/PARSEC/parsec-3.0/bin/parsecmgmt"
-cmd = path + " -a run -p bodytrack -i {} -n {} -c gcc-openmp -s 'sudo taskset --all-tasks {}'"
+path = "/home/odroid/hw1_files/parsec_files/"
+inputs = [path + 'sequenceB_261/', path + 'in_10M_blackscholes.txt']
+cmd = 'taskset --all-tasks ' + '{} {}'
+
+benchmarks = [ path + 'bodytrack', path + 'blackscholes' ]
 
 random.seed()
 while True:
@@ -22,17 +24,23 @@ while True:
 	# Roll a die:
 	d = random.random()
 	if d >= bench_thresh and len(bench_processes) == 0:
-		print("hi")
+		print("starting new run")
 		# launch a new benchmark
 		affinity_l = 0#random.randint(0, 15)
 		affinity_b = random.randint(1, 15)
 		affinity = affinity_l | (affinity_b << 4)
 		affinity_string = hex(affinity)
-		input_s = input_size[random.randint(0, len(input_size)-1)]
 		num_threads = bin(affinity).count("1")#random.randint(1, bin(affinity).count("1"))
-		cmd_f = cmd.format(input_s, num_threads, affinity_string)
-		print(cmd_f)
-		bench_processes.append(subprocess.Popen(['/bin/sh', "./parsec.sh", input_s, str(num_threads), affinity_string], stdout=FNULL ))
+		bm_index = 1#random.randint(0,1)
+		if bm_index == 0: # bodytrack
+			cmd_bm_args = ' {} 4 260 3000 8 3 {} 0'.format(inputs[bm_index], num_threads)
+			#cmd_f = cmd.format(affinity_string, cmd_bm)
+		elif bm_index == 1: # Blackscholes
+			cmd_bm_args = ' {} {} /dev/null'.format(num_threads, inputs[bm_index])
+			#cmd_f = cmd.format(affinity_string, cmd_bm)
+		#print(cmd_f.split(' '))
+		bench_processes.append(subprocess.Popen(['/bin/sh', './parsec.sh', affinity_string, benchmarks[bm_index], \
+				cmd_bm_args]))#, stdout=FNULL ))
 
 
 	time.sleep(random.randint(0, period_max))
