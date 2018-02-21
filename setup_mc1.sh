360731bb392bbb98bd9bd7e5a0999df5a9affe9b
Mark Blanco <mark.p.blanco@gmail.com>
2018-02-21 17:19:02 -0500

    Update permissions for parsec benchmarks in setup script

--- setup_mc1.sh
+++ setup_mc1.sh
@@ -65,6 +65,7 @@ mkdir /home/st2/hw1_files
 unzip -o files_for_students.zip -d /home/st2/hw1_files
 sudo chmod -R st1:st1 /home/st1
 sudo chmod -R st2:st2 /home/st2
+sudo chmod +x /home/st*/hw1_files/parsec_files/b*
 # make sysfs accessible to all in ncps_students (using crontab so this occurs at each reboot)
 #sudo crontab -u root -l | { cat; echo "chown -R root:ncps_students /sys 2> /dev/null"; } | sudo crontab -u root
 #sudo crontab -u root -l | { cat; echo "chmod -R 775 /sys 2> /dev/null"; } | sudo crontab -u root
